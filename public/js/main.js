c20d56744703208dc406124c885cbc5ee8518865
Aaron Martin <grav3serker@gmail.com>
2016-09-14 00:23:45 -0700

    final touches

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+// var images=['/images/alien.jpg',
+//             '/images/moon.jpg',
+//             '/images/space.jpg'];
+//
+// var randomNumber = Math.floor(Math.random() * images.length);
+// var bgImg = 'url(' + images[randomNumber] + ')';
+//
+// $('body').css({'background':bgImg, 'background-size':'cover', });
+//
+// });
